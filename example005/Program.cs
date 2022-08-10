4c0502b8e4439bc11b4e1271a5b7befefb6111ff
unknown <artem03051987@gmail.com>
2022-08-11 01:19:48 +0500

    Tolower()

---
+++ example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя:");
+string myname = Console.ReadLine();
+if(myname.ToLower() == "артём")
+{
+  Console.WriteLine ("Ура,это же Артём!!!");
+}
+else 
+{
+    Console.Write("Привет,");
+    Console.WriteLine(myname);
+}
