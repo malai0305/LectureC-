1d7d62d1420da84e98286d552abebcd8187bad39
unknown <artem03051987@gmail.com>
2022-08-11 00:35:20 +0500

    новое случайное число new Random

---
+++ example004/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https:// новое случайное число.
+int a = new Random().Next(1, 10);
+Console.WriteLine(a);
+int b = new Random().Next(35, 1000);
+Console.WriteLine(b);
+Console.WriteLine(a*b);
