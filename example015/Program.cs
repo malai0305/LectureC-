26faee242e327baca69d3672764977f8088151b3
unknown <artem03051987@gmail.com>
2022-08-20 01:01:59 +0500

    массив,функции

---
+++ example015/Program.cs
@@ -0,0 +1,15 @@
+﻿//нахождение числа по индесу в массиве,функции.
+int[] array = {13,45,8,70,94,93,7,49,41,78,39,50};
+int n = array.Length;
+int find =13;
+
+int index = 0;
+while (index<n)
+{
+    if(array[index]==find)
+    {
+       Console.WriteLine(index);
+
+    }
+    index++;
+}
