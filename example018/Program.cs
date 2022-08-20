5be5391ce0ec608045ea33da9042b0b654d535f0
unknown <artem03051987@gmail.com>
2022-08-20 16:43:48 +0500

    цикл for,цикл в цикле

---
+++ example018/Program.cs
@@ -0,0 +1,27 @@
+﻿// новый цикл for ,не только while.
+
+for(int i = 0;i<10;i++)
+{
+    Console.WriteLine(i);
+}
+
+//Цикл в цикле.
+for(int i =0;i<10;i++)
+{
+    for (int j=0;j<10;j++)
+    {
+        Console.WriteLine(i*j);
+    } 
+    Console.WriteLine();
+}
+//Вывод таблицы умножения на экран.
+
+for(int i = 2;i<=10;i++)
+{
+    for(int j = 2;j<=10;j++)
+    {
+        Console.WriteLine($"{i}*{j}={i*j}");
+    }
+    Console.WriteLine();
+}
+//
