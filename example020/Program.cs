fa7f4be4e529937da343398a578ee92719d706ca
unknown <artem03051987@gmail.com>
2022-08-21 16:12:46 +0500

    цикл в цикле ,сортировка

---
+++ example020/Program.cs
@@ -0,0 +1,20 @@
+﻿// Сортировка методом выбора.Цикл в цикле.
+
+void SelectionSort (int[]array)
+{
+    for(int i = 0;i<array.Lendyh-1;i++)
+    {
+        int Position = i;
+        for(int j =i+1;j<array.Length;j++)
+        {
+            if(array[j]<array[minPosition])
+            {
+                minPosition = j;
+            }
+        }
+
+int temporary = Array[i];
+Array[i] = Array[minPosition];
+array[minPosition] = temporary;
+    }
+}
