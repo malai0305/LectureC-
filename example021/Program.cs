10e9b368779f9559524198fff9e293e2631fd516
unknown <artem03051987@gmail.com>
2022-08-26 21:59:13 +0500

    Order Sort

--- example021/Program.cs
+++ example021/Program.cs
@@ -1,29 +1,33 @@
 ﻿//сортировка методом выбора.
 
  int []arr={1,2,6,7,4,8,5,9,3,7,4,5,8};
- void PrintArray(int[]array)
+ void PrintArray(int[]array)//подзадача вывода массива в консоль
  {
     int count = array.Length;
     for(int i = 0;i<count;i++)
+ 
     {
-        Console.WriteLine($"{array[i]}");
+        Console.Write($"{array[i]} ");
     }
     Console.WriteLine();
  }
- void SelectionSort(int[]array)
+ 
+ 
+
+void OrderSort(int[]array)
  {
-    for(int i=0;i<array.Length-1;i++)
+ for(int i=0;i<array.Length-1;i++)
     {
         int minPosition = i;
         for(int j = i+1;j<array.Length;j++)
         {
             if(array[j]<array[minPosition])minPosition = j;
-        }
+      }
         int temporary = array[i];
         array[i]=array[minPosition];
         array[minPosition]=temporary;
     }
  }
  PrintArray(arr);
- SelectionSort(arr);
+ OrderSort(arr);
 PrintArray(arr);
