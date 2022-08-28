62e7f37eb1ad9a773f4f76d08a71b847029d1c1c
unknown <artem03051987@gmail.com>
2022-08-28 17:08:01 +0500

    вычесления факториала,при помощи рекурсии

---
+++ example026/Program.cs
@@ -0,0 +1,14 @@
+﻿//Вычисления факториала ,при помощи рекурсии.
+
+double Factorial(int n)
+{
+    //1i=1
+    //0i=1
+    if(n==1)return 1;
+    else return n*Factorial(n-1);
+}
+Console.WriteLine(Factorial(3));
+for (int i=1;i<40;i++)
+{
+    Console.WriteLine($"{i}i={Factorial(i)}");
+}
